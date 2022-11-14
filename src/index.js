49b999427ee9775aad08424c05dd63b2db75426d
Rayandson <rayandson.silva321@gmail.com>
2022-11-14 00:20:07 -0300

    versao final

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import cors from "cors"
 import { MongoClient } from "mongodb";
 import joi from "joi";
 import dayjs from "dayjs";
+import dotenv from "dotenv";
 
 const participantesSchema = joi.object({
     name: joi.string().required()
@@ -14,14 +15,11 @@ const mensagensSchema = joi.object({
     from: joi.required()
 })
 
-
-
-// console.log((dayJsObject.format('hh:mm:ss'))
-
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-const mongoClient = new MongoClient("mongodb://localhost:27017") 
+const mongoClient = new MongoClient(process.env.MONGO_URI) 
 let db;
 
 try {
@@ -32,7 +30,7 @@ try {
 }
 
 
-app.post("/participantes", async (req, res) => {
+app.post("/participants", async (req, res) => {
     const dayJsObject = dayjs();
     const validation = participantesSchema.validate(req.body, { abortEarly: false })
     const { name } = req.body;
@@ -89,24 +87,60 @@ app.post("/messages", async (req, res) => {
 })
 
 app.get("/messages", async (req, res) => {
-    try {
-        const messages = await db.collection("mensagens").find().toArray()
-        res.status(200).send(messages)
-    } catch {
-        res.sendStatus(500)
+    const limit = parseInt(req.query.limit)
+    const user = req.headers.user
+    if(limit) {
+        try {
+            const messages = await db.collection("mensagens").find({$or: [{to: user, type: "private_message"},{type:"message"} ]}).toArray()
+            res.status(200).send(messages.slice(0,limit))
+        } catch {
+            res.sendStatus(500)
+        }
     }
+    else {
+        try {
+            const messages = await db.collection("mensagens").find({$or: [{to: user, type: "private_message"},{type:"message"} ]}).toArray()
+            res.status(200).send(messages)
+        } catch {
+            res.sendStatus(500)
+        }
+    }
+   
     
 })
 
 app.post("/status", async (req, res) => {
     const user = await db.collection("participantes").findOne({name: req.headers.user})
+    
     if(!user) {
         res.sendStatus(404)
         return
     }
-    await db.collection("status").insertOne("")
+    try {
+        await db.collection("participantes").updateOne({name: user.name},{$set: {lastStatus: Date.now()}})
+        res.sendStatus(200)
+    } catch {
+        res.sendStatus(400)
+    }
 })
 
+// async function IdentificarInativos() {
+//     let mensagensStatus = []
+//     const dayJsObject = dayjs();
+//     try {
+//         const inativos = await db.collection("participantes").find({lastStatus: {$lt: Date.now() - 10000}}).toArray()
+//         await db.collection("participantes").deleteMany({lastStatus: {$lt: Date.now() - 10000}})
+//         inativos.forEach((i) => {
+//             mensagensStatus.push({from: i.name, to: 'Todos', text: 'sai da sala...', type: 'status', time: dayJsObject.format('hh:mm:ss')})
+//             console.log(mensagensStatus)
+//         })
+//         console.log(mensagensStatus)
+//         await db.collection("mensagens").insertMany(mensagensStatus)
+//     } catch {
+//         console.log("0 participantes inativos")             
+//     }
+// }     
 
+// setInterval(IdentificarInativos, 25000)
 
 app.listen(5000, () => console.log("app running in port 5000"))
